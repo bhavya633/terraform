e28cf247f9b1e33fe68abbae9022d292d65aa60c
bhavya633 <57653677+bhavya633@users.noreply.github.com>
2020-09-25 03:30:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 locals {
   is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a){1}\\..*$/", "1") == "1" ? true : false
 }
+provider "aws" {
+ aws_region   = var.region
+}
 
 resource "aws_instance" "this" {
   count = var.instance_count
-  aws_region       = var.region
   ami              = var.ami
   instance_type    = var.instance_type
   user_data        = var.user_data
